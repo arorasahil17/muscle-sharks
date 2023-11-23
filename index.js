612bb4f5ca947efa098a65993d96a6a59f8d3dd9
= <=>
2023-11-23 12:55:00 +0530

    dist changes

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const express=require("express");
+const express = require("express");
 const dbConnection = require("./database");
-const cookieParser=require("cookie-parser");
-const cors=require("cors");
-const path=require("path")
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
+const userRouter = require("./routes/userRouter");
+const productRoutes = require("./routes/productRoutes");
+const orderRouter = require("./routes/orderRouter");
+const couponRouter = require("./routes/couponRouter");
+const cartRouter = require("./routes/cartRouter");
+const adminRouter = require("./routes/adminRouter");
 
-const userRouter=require("./routes/userRouter");
-const productRoutes=require("./routes/productRoutes");
-const orderRouter=require("./routes/orderRouter")
-const couponRouter=require("./routes/couponRouter");
-const cartRouter=require("./routes/cartRouter");
-const adminRouter=require("./routes/adminRouter")
-
-
-const app=express();
+const app = express();
 dbConnection();
-app.use(express.static(path.resolve(__dirname,"dist")))
+app.use(express.static(path.resolve(__dirname, "dist")));
 
-app.use(cors({
-    credentials:true,
-    origin:["http://localhost:5173","http://localhost:5174", 'https://ms-server-six.vercel.app/']
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"dist",'index.html'))
-})
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 // app.use(require("./routes/userRouter"));
 // app.use(require("./routes/productRoutes"));
@@ -37,14 +36,13 @@ app.get("/",(req,res)=>{
 // app.use(require("./routes/adminRouter"))
 // app.use(require("./routes/couponRouter"));
 
-app.use("/api",userRouter)
-app.use("/api",productRoutes)
-app.use("/api",cartRouter)
-app.use("/api",orderRouter)
-app.use("/api",adminRouter)
-app.use("/api",couponRouter)
+app.use("/api", userRouter);
+app.use("/api", productRoutes);
+app.use("/api", cartRouter);
+app.use("/api", orderRouter);
+app.use("/api", adminRouter);
+app.use("/api", couponRouter);
 
-
-
-
-app.listen(process.env.PORT || 5000,()=>console.log(`Server is running on PORT ${process.env.PORT} ........`));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server is running on PORT ${process.env.PORT} ........`)
+);
